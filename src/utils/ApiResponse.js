a8c93e22fdf09f29031e61ceeb53d87514b6f8a5
Geetesh <name>
2024-10-19 20:29:31 +0530

    Api Handling

---
+++ src/utils/ApiResponse.js
@@ -0,0 +1,16 @@
+class ApiResponse{
+    constructor(statusCode,data,message="success"){
+        this.statusCode  = statusCode
+        this.data = data
+        this.message = message
+        this.success = statusCode
+         
+        /**
+        informational Response (100-199)
+        successful response (200-300)
+        redireciton message (300-399)
+        client error responses (400-499)
+        server error responses(500-599)
+         */
+    }
+}
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,32 @@
+const asyncHandler = (requestHandler)=>{
+    (req,res,next)=>{
+        Promise.resolve(requestHandler(req,res,next))
+        .catch((err)=>next(err))
+        
+    }
+}
+
+
+
+export {asyncHandler}
+
+// const asyncHandler = ()=>{}
+// const asyncHandler = (func)=>()=>{}
+// const asyncHandler = (func)=>async()=>{}
+// const asyncHandler = (fn)=>()=>{}
+
+    // wrapper funcntion that we are using in production 
+    // using try and catch but we will use asyn await tpe 
+   /* const asyncHandler =(fn)=>async(req,res,next)=>{
+        try {
+            await fn(req,res,next)
+            
+        } catch (error) {
+            res.status(error.code || 500).json({
+                success:false,
+                message:error.message
+            })
+            
+        }
+
+    }*/
